f9e57a5b43f8d41cbfa985197d341811824f4be3
Artur <79041911+Fateyv@users.noreply.github.com>
2021-07-30 09:55:36 +0300

    added all

---
+++ controller/TodosController.js
@@ -0,0 +1,42 @@
+class TodosController {
+    constructor($el) {
+       this.initCollection();
+       this.initView($el);
+    }
+
+    initCollection() {
+        this.todosCollection = new TodosCollection(TODOS_URL);
+        this.todosCollection
+        .fetchTodos()
+        .then (() => this.renderList());
+    }
+
+    initView($el) {
+        this.todosView = new TodosView($el, {
+            onDelete: this.deleteTodo.bind(this),
+            onAddTodo: this.addTodo.bind(this),
+            onToggle: this.toggleTodo.bind(this)
+        });
+    }
+
+    renderList() {
+        this.todosView.renderList(this.todosCollection.list);
+    }
+
+    deleteTodo(id) {
+        this.todosCollection.deleteTodo(id);
+
+        this.renderList();
+    }
+
+    addTodo(data) {
+        this.todosCollection.addTodo(data)
+        .then(() => this.renderList());
+    }
+
+    toggleTodo(id) {
+        this.todosCollection.toggleTodo(id);
+
+        this.renderList();
+    }
+}
